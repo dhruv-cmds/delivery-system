49c108f70a1b70586597edce88c0c344c10aa8d8
Dhruv-Cmds <pd1900565@gmail.com>
2026-05-11 11:29:56 +0530

    Add SQLAlchemy base setup and update delivery Docker compose services

---
+++ backend/app/db/session.py
@@ -0,0 +1,59 @@
+import os 
+
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncSession
+)
+
+from sqlalchemy.orm import sessionmaker
+
+from dotenv import load_dotenv
+
+from urllib.parse import quote_plus
+
+from app.core import ENV
+
+load_dotenv()
+
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = quote_plus(
+    os.getenv("DB_PASSWORD") or ""
+)
+DB_NAME = os.getenv("DB_NAME")
+
+
+if ENV == "docker":
+    DB_HOST = "delivery-db"
+    DB_PORT = "3306"
+
+else:
+    DB_HOST = "127.0.0.1"
+    DB_PORT = "3008"
+
+
+DATABASE_URL = (
+    f"mysql+aiomysql://"
+    f"{DB_USER}:{DB_PASSWORD}"
+    f"{DB_HOST}:{DB_PORT}"
+)
+
+
+engine = create_async_engine (
+
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_size=20,
+    max_overflow=30,
+    pool_timeout=30,
+    echo=False
+)
+
+
+AsyncSessionLocal = sessionmaker (
+    
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False
+)
