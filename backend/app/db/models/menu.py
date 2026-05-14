b387ff106002d8332e7483c4c16cf6bfa462ff41
Dhruv-Cmds <pd1900565@gmail.com>
2026-05-14 17:24:02 +0530

    feat(menu): add Menu model with restaurant relationship

---
+++ backend/app/db/models/menu.py
@@ -0,0 +1,52 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    DECIMAL,
+    ForeignKey
+)
+
+from sqlalchemy.orm import relationship
+
+from app.db import Base
+
+class Menu(Base):
+
+    __tablename__ = "menu_items"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+
+    restaurant_id = Column(
+        Integer,
+        ForeignKey("restaurants.id"),
+        nullable=False
+    )
+
+    item_name = Column(
+        String(255),
+        nullable=False,  
+    )
+
+    description = Column(
+        String(255),
+        nullable=True
+    )
+
+    price = Column(
+        DECIMAL(10 , 2),
+        nullable=False
+    )
+
+    status = Column(
+        String(20),
+        default="AVAILABLE"
+    )
+
+    restaurant = relationship(
+        "Restaurant",
+        back_populates="menu_items"
+    )
