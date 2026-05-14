736cfc4973225c0abf4aab0536dd58695f5cda13
Dhruv-Cmds <pd1900565@gmail.com>
2026-05-14 17:08:05 +0530

    Added relationship with restaurant

--- backend/app/db/models/user.py
+++ backend/app/db/models/user.py
@@ -1,4 +1,12 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import (
+    Column, 
+    Integer, 
+    String, 
+    DateTime
+)
+
+from sqlalchemy.orm import relationship
+
 from datetime import datetime
 
 from app.db import Base
@@ -7,13 +15,57 @@ class User(Base):
 
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(100), unique=True, nullable=False)
-    name = Column(String(100), nullable=False)
-    email = Column(String(255), unique=True, nullable=False)
-    hashed_password = Column(String(255), nullable=False)
-    role = Column(String(50), default="CUSTOMER", nullable=False)
-    phone = Column(String(30), unique=True, nullable=False)
-    is_active = Column(String(50), default="ACTIVE")
-    timestamps = Column(DateTime, default=datetime.utcnow)
+    id = Column(
+        Integer, 
+        primary_key=True, 
+        index=True
+    )
+
+    username = Column(
+        String(100), 
+        unique=True, 
+        nullable=False
+    )
+
+    name = Column(
+        String(100), 
+        nullable=False
+    )
+
+    email = Column(
+        String(255), 
+        unique=True, 
+        nullable=False
+    )
+
+    hashed_password = Column(
+        String(255), 
+        nullable=False
+    )
+
+    role = Column(
+        String(50), 
+        default="CUSTOMER", 
+        nullable=False
+    )
+
+    phone = Column(
+        String(30), 
+        unique=True, 
+        nullable=False
+    )
+
+    is_active = Column(
+        String(50), 
+        default="ACTIVE"
+    )
+
+    timestamps = Column(
+        DateTime, 
+        default=datetime.utcnow
+    )
     
+    restaurants = relationship (
+        "Restaurant",
+        back_populates="owner"
+    )
