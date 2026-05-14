aa25fcf996134c3077c24e299044b37d51989abc
Dhruv-Cmds <pd1900565@gmail.com>
2026-05-14 17:05:12 +0530

    feat: introduce restaurant  model and persistence schema

---
+++ backend/app/db/models/restaurant.py
@@ -0,0 +1,81 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    String, 
+    DateTime,
+    ForeignKey
+)
+
+from sqlalchemy.orm import relationship
+
+from app.db import Base
+
+class Restaurant(Base):
+
+    __tablename__ = "restaurants"
+
+    id = Column(
+        Integer, 
+        primary_key=True, 
+        index=True
+    )
+
+    name = Column(
+        String(255), 
+        nullable=False
+    )
+    
+    address = Column(
+        String(255), 
+        nullable=False
+    )
+
+    phone = Column(
+        String(20), 
+        unique=True, 
+        nullable=False
+    )
+
+    status = Column(
+        String(50), 
+        default="OPEN",
+        nullable=False
+    )
+
+    owner_id = Column(
+        Integer, 
+        ForeignKey("User.id"),
+        nullable=True
+
+    )
+    
+    created_at = Column(
+        DateTime, 
+        default=datetime.utcnow,
+        nullable=False
+    )
+
+    updated_at = Column(
+        DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+        nullable=False
+    )
+
+    owner = relationship(
+        "User",
+        back_populates="restaurant",
+    )
+
+    # orders = relationship(
+    #     "Order",
+    #     back_populates="restaurant"
+    # )
+    
+    # menu_items = relationship(
+    #     "Menu",
+    #     back_populates="restaurant",
+    #     cascade="all, delete-orphan"
+    # )
