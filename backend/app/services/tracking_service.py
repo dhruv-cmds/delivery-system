d8fe6a710814e87a3a641dc610f072f117668855
Dhruv-Cmds <pd1900565@gmail.com>
2026-06-01 08:27:51 +0530

    Added tracking_service in services

---
+++ backend/app/services/tracking_service.py
@@ -0,0 +1,53 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.db.models import OrderTracking
+from app.core import (
+    DatabaseError,
+    logger
+)
+
+
+async def create_tracking(
+    db: AsyncSession,
+    order_id: int,
+    latitude,
+    longitude,
+):
+    tracking = OrderTracking(
+        order_id=order_id,
+        latitude=latitude,
+        longitude=longitude,
+    )
+
+    try:
+        db.add(tracking)
+
+        await db.commit()
+
+        await db.refresh(tracking)
+
+        return tracking
+
+    except Exception:
+
+        await db.rollback()
+
+        logger.exception(
+            "Unexpected error while creating tracking"
+        )
+
+        raise DatabaseError()
+    
+async def get_tracking_by_order(
+        db: AsyncSession,
+        order_id: int,
+    ):
+
+    result = await db.execute(
+        select(OrderTracking)
+        .where(OrderTracking.order_id == order_id)
+        .order_by(OrderTracking.created_at.desc())
+    )
+
+    return result.scalars().all()
